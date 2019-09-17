706b1753bfe01eb8e5f0f97db08271577eeff1ea
Paul Johnson <paul.johnson@parkersoftware.com>
2019-09-17 16:07:34 +0100

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,32 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, watch, parallel } = require("gulp");
 var eslint = require("gulp-eslint");
 var del = require("del");
+var sass = require("gulp-sass");
+const browsersync = require("browser-sync").create();
+
+function browserSync(done) {
+    browsersync.init({
+      server: {
+        baseDir: "./dist/"
+      },
+      port: 3000
+    });
+    done();
+  }
+  
+  function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+  }
 
 function clean() {
-    return del(["dist/**/*.js", "dist/**/*.css", "dist/**/*.html","dist/**/*.png","dist/**/*.less"]);
+    return del(["dist/**/*.*"]);
+}
+
+function scss(){
+    return src("./src/style/scss/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./dist/assets/css/"));
 }
 
 function lintjs() {
@@ -13,8 +36,49 @@ function lintjs() {
     .pipe(eslint.failAfterError());
 }
 
+function moveHTML(){
+    return src("./src/html/*.*")
+    .pipe(dest("./dist/"));
+}
+
+function moveImages(){
+    return src("./src/assets/images/*.*")
+    .pipe(dest("./dist/assets/images/"));
+}
+
+function moveFonts(){
+  return src("./src/assets/webfonts/*.*")
+  .pipe(dest("./dist/assets/webfonts/"));
+}
+
+function moveVendor(){
+  return src("./node_modules/bulma-extensions/bulma-quickview/dist/js/bulma-quickview.min.js")
+  .pipe(dest("./dist/assets/js/"));
+}
+
+function watchFiles() {
+    watch("./src/style/**/*", scss);
+    watch("./src/assets/images/*", moveImages);
+    watch("./src/html/*", moveHTML);
+    watch(
+      [
+        "./dist/assets/**/*",
+        "./dist/*"
+      ],
+      series(browserSyncReload)
+    );    
+  }
+
+
+const monitor = parallel(watchFiles, browserSync);
 exports.clean = clean;
 exports.lintjs = lintjs;
+exports.scss = scss;
+exports.moveHTML = moveHTML;
+exports.moveImages = moveImages;
+exports.monitor = monitor;
+exports.moveFonts = moveFonts;
+exports.moveVendor = moveVendor;
 
-exports.default = series(clean, lintjs);
+exports.default = series(clean, scss, lintjs, [moveHTML, moveImages,moveFonts, moveVendor]);
 
