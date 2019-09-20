b9ea9dc9550e163be58ad30a9127b5b82ea19a12
Jamie <jamie.mascall@parkersoftware.com>
2019-09-20 11:43:24 +0100

    Removed activeChat from Store

--- src/assets/store.js
+++ src/assets/store.js
@@ -90,19 +90,6 @@
             userChanged(state, user) {
                 console.log(user);
             },
-            setActiveChat(state, activeChat) {
-                Object.keys(state.chats).forEach((key) => {
-                    var chat = state.chats[key];
-                    if(chat.ChatUID == activeChat.ChatUID) {
-                        chat.IsActiveChat = true;
-                        state.currentChat = chat;
-                    } else {
-                        chat.IsActiveChat = false;
-                    }
-                });
-
-                console.log(state.chats);
-            },
             saveLoginDetails(state, loginDetails) {
                 state.userName = loginDetails.userName;
                 state.password = loginDetails.password;
