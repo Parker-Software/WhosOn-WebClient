6787904b510a07e6a6308cb9e61ac97ed6469637
Paul Johnson <paul.johnson@parkersoftware.com>
2019-09-23 15:28:43 +0100

    More tweaks to layout

--- src/components/chat/chatConversationSurvey.js
+++ src/components/chat/chatConversationSurvey.js
@@ -1,23 +1,9 @@
 (function(services){
     Vue.component('chatConversationSurvey', {
         template: `
-        <div class="survey-block">
-            <div class="container is-fluid">
-                <h3 class="subtitle is-4">Survey Results</h3>
-                <div class="columns is-gapless">
-                    <div class="column is-6">
-                        <span class="is-flex"><strong>{surveyfield}</strong>: {surveyresult}</span>
-                        <span class="is-flex"><strong>{surveyfield}</strong>: {surveyresult}</span>
-                        <span class="is-flex"><strong>{surveyfield}</strong>: {surveyresult}</span>
-                        <span class="is-flex"><strong>{surveyfield}</strong>: {surveyresult}</span>
-                    </div>
-                    <div class="column is-6">
-                        <span class="is-flex"><strong>{surveyfield}</strong>: {surveyresult}</span>
-                        <span class="is-flex"><strong>{surveyfield}</strong>: {surveyresult}</span>
-                        <span class="is-flex"><strong>{surveyfield}</strong>: {surveyresult}</span>
-                        <span class="is-flex"><strong>{surveyfield}</strong>: {surveyresult}</span>
-                    </div>
-                </div>
+        <div class="survey-block columms">
+            <div class="column is-full">        
+            <span class="is-flex" v-for="item in this.$store.state.currentChatPreSurveys"><strong>{{ item.Name }}</strong>: {{ item.Value }}</span>
             </div>
         </div>
         `
