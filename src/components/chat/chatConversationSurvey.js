911904e6fa89d9f32c9d19bd0463c63380f277a3
Daniel Tallentire <daniel@parkersoftware.com>
2019-09-23 17:02:40 +0100

    survey block position bleed fix

--- src/components/chat/chatConversationSurvey.js
+++ src/components/chat/chatConversationSurvey.js
@@ -2,7 +2,7 @@
     Vue.component('chatConversationSurvey', {
         template: `
         <div class="survey-block columns">
-            <div class="column is-full">
+            <div class="column">
                 <span class="is-flex" v-for="item in this.$store.state.currentChatPreSurveys"><strong>{{ item.Name }}</strong>: {{ item.Value }}</span>
             </div>
         </div>
